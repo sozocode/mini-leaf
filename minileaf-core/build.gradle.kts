a237106a3ccd6fd9f763cc30717f643a52a98872
SozoCode <sozocode@users.noreply.github.com>
2025-12-17 00:07:14 -0800

    Remove internal publishing configuration from subprojects

--- minileaf-core/build.gradle.kts
+++ minileaf-core/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm")
-    `maven-publish`
 }
 
 dependencies {
@@ -24,32 +23,4 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
     testImplementation("io.mockk:mockk:1.13.9")
     testImplementation("org.assertj:assertj-core:3.25.1")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            groupId = "com.sozocode"
-            artifactId = "mini-leaf-core"
-            version = project.version.toString()
-        }
-    }
-
-    repositories {
-        maven {
-            name = "nexus"
-            url = if (version.toString().endsWith("SNAPSHOT")) {
-                uri("http://nexus.sozocode.com/repository/maven-snapshots/")
-            } else {
-                uri("http://nexus.sozocode.com/repository/maven-releases/")
-            }
-            isAllowInsecureProtocol = true
-            credentials {
-                username = project.findProperty("nexusUsername") as String? ?: ""
-                password = project.findProperty("nexusPassword") as String? ?: ""
-            }
-        }
-    }
 }
--- minileaf-jackson/build.gradle.kts
+++ minileaf-jackson/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm")
-    `maven-publish`
 }
 
 dependencies {
@@ -15,32 +14,4 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testImplementation("org.assertj:assertj-core:3.25.1")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            groupId = "com.sozocode"
-            artifactId = "mini-leaf-jackson"
-            version = project.version.toString()
-        }
-    }
-
-    repositories {
-        maven {
-            name = "nexus"
-            url = if (version.toString().endsWith("SNAPSHOT")) {
-                uri("http://nexus.sozocode.com/repository/maven-snapshots/")
-            } else {
-                uri("http://nexus.sozocode.com/repository/maven-releases/")
-            }
-            isAllowInsecureProtocol = true
-            credentials {
-                username = project.findProperty("nexusUsername") as String? ?: ""
-                password = project.findProperty("nexusPassword") as String? ?: ""
-            }
-        }
-    }
 }
